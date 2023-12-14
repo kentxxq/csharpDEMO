fa0523eb2e3c5bf19f9cf952bc874cc0b5161b4c
kentxxq <805429509@qq.com>
2023-12-14 17:09:18 +0800

    一个登录demo

---
+++ Aspnetcore/AddSwaggerWithJWT/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+using AddSwaggerWithJWT.Response;
+using AddSwaggerWithJWT.RO;
+using AddSwaggerWithJWT.SO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AddSwaggerWithJWT.Controllers;
+
+/// <summary>用户相关</summary>
+public class UserController : ControllerBase
+{
+    private readonly JWTService _jwtService;
+    private readonly ILogger<UserController> _logger;
+
+    /// <inheritdoc/>
+    public UserController(ILogger<UserController> logger, JWTService jwtService)
+    {
+        _logger = logger;
+        _jwtService = jwtService;
+    }
+
+
+    public ResultModel<LoginSO> Login(LoginRO loginRO)
+    {
+        if (loginRO.Username != "admin" || loginRO.Password != "123456")
+        {
+            return ResultModel<LoginSO>.Error("登录失败", new LoginSO());
+        }
+
+        var token = _jwtService.GetToken(1, "admin");
+        return ResultModel<LoginSO>.Ok(new LoginSO { Token = token });
+    }
+}
---
+++ Aspnetcore/AddSwaggerWithJWT/SO/LoginSO.cs
@@ -0,0 +1,6 @@
+namespace AddSwaggerWithJWT.SO;
+
+public class LoginSO
+{
+    public string Token { get; set; }
+}
