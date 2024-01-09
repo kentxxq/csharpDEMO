526a2f0b5345155d38437918b89469faca90bc18
kentxxq <805429509@qq.com>
2024-01-09 17:10:58 +0800

    feat: 调试logfmt格式

--- Aspnetcore/AddSerilog/Program.cs
+++ Aspnetcore/AddSerilog/Program.cs
@@ -1,5 +1,5 @@
-using Microsoft.AspNetCore.Identity;
 using Serilog;
+using Serilog.Logfmt;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +8,21 @@ Log.Logger = new LoggerConfiguration()
     .Enrich.When(logEvent=>!logEvent.Properties.ContainsKey("SourceContext"),enrichmentConfig=>enrichmentConfig.WithProperty("SourceContext","SourceContext"))
     .Enrich.When(logEvent=>!logEvent.Properties.ContainsKey("ThreadName"),enrichmentConfig=>enrichmentConfig.WithProperty("ThreadName","ThreadName"))
     .ReadFrom.Configuration(builder.Configuration)
+    // logfmt格式我只用于写入到控制台，文件都建议采用json格式。
+    // logfmt对比竖线分隔符，优势如下：
+    // - 如果需要输出所有的字段，而不确定有哪些字段的时候。例如sourceContext
+    // - 字段很多，记不住顺序。而key-value的展示起来很清晰
+
+    // 要使用下面的配置，需要注释掉json配置中的console配置
+    // .WriteTo.Console(formatter:new LogfmtFormatter(opt =>
+    // {
+    //     // 双引号用单引号代替
+    //     opt.OnDoubleQuotes(q => q.ConvertToSingle());
+    //     // 打印所有的额外sourceContext
+    //     opt.IncludeAllProperties();
+    //     // 保持原有的大小写模式。默认会变成下划线分割，例如XiaoLi变成xiao_li
+    //     opt.PreserveCase();
+    // }))
     .CreateLogger();
 
 Log.Information("启动中...");
