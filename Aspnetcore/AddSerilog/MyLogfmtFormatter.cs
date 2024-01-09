059ba3c99edfe7a835ead9ad2fbc5c64ae6c1ac2
kentxxq <805429509@qq.com>
2024-01-09 18:20:16 +0800

    feat: 使用json配置替代

---
+++ Aspnetcore/AddSerilog/MyLogfmtFormatter.cs
@@ -0,0 +1,18 @@
+﻿using Serilog.Formatting;
+using Serilog.Formatting.Json;
+using Serilog.Logfmt;
+
+namespace AddSerilog;
+
+public static class MyLogfmtFormatter
+{
+    public static ITextFormatter Formatter {get;} = new LogfmtFormatter(opt =>
+    {
+        // 双引号用单引号代替
+        opt.OnDoubleQuotes(q => q.ConvertToSingle());
+        // 打印所有的额外sourceContext
+        opt.IncludeAllProperties();
+        // 保持原有的大小写模式。默认会变成下划线分割，例如XiaoLi变成xiao_li
+        opt.PreserveCase();
+    });
+}
