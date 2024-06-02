c26ce83668a022848dffa78902b279adc63e26e0
kentxxq <admin@kentxxq.com>
2024-06-02 13:48:40 +0800

    解析proxy地址

--- PackageUsage/Net/MyNetTools/StaticNetTool.cs
+++ PackageUsage/Net/MyNetTools/StaticNetTool.cs
@@ -9,6 +9,24 @@ namespace MyNetTools;
 
 public static class StaticNetTool
 {
+    public static WebProxy GetWebproxyFromString(string url)
+    {
+        // 拿到proxy的用户名和密码
+        var uri = new Uri(url);
+        var username = uri.UserInfo.Split(':')[0];
+        var password = uri.UserInfo.Split(':')[1];
+        // 去掉url中的用户名密码
+        var uriBuilder = new UriBuilder(uri)
+        {
+            UserName = null,
+            Password = null
+        };
+        return new WebProxy(uriBuilder.Uri)
+        {
+            Credentials = new NetworkCredential(username, password)
+        };
+    }
+
     /// <summary>
     /// 获取本机ipv4内网ip<br />
     /// 如果网络不可用返回127.0.0.1<br />
