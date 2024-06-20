60eac374d3db79ac67f1d59af25101f53bef1b47
kentxxq <805429509@qq.com>
2024-06-20 17:10:48 +0800

    简化

--- Aspnetcore/AddOpentelemetry/MyOpenTelemetryExtension.cs
+++ Aspnetcore/AddOpentelemetry/MyOpenTelemetryExtension.cs
@@ -18,7 +18,7 @@ public static class MyOpenTelemetryExtension
     /// <summary>添加OpenTelemetry</summary>
     /// <param name="builder"></param>
     /// <returns></returns>
-    public static WebApplicationBuilder AddMyOpenTelemetry(this WebApplicationBuilder builder)
+    public static void AddMyOpenTelemetry(this WebApplicationBuilder builder)
     {
         // 不要开代理!!!!!!
         // 不要开代理!!!!!!
@@ -26,67 +26,28 @@ public static class MyOpenTelemetryExtension
         _ocEndpoint = builder.Configuration["OC_Endpoint"] ??
                       throw new InvalidOperationException("必须配置open telemetry的collector地址");
 
+        // 统一配置名称,版本,连接地址
         var openTelemetryBuilder = builder.Services.AddOpenTelemetry()
-            // .UseOtlpExporter(OtlpExportProtocol.Grpc,new Uri(_ocEndpoint))
+            .UseOtlpExporter(OtlpExportProtocol.Grpc,new Uri(_ocEndpoint))
             .ConfigureResource(resourceBuilder => resourceBuilder.AddService(AppName, serviceVersion: AppVersion));
 
-        // AddLogging(builder);
-        AddLogging(openTelemetryBuilder);
-        AddMetrics(openTelemetryBuilder);
-        AddTrace(openTelemetryBuilder);
+        // 日志
+        openTelemetryBuilder.WithLogging();
 
-        return builder;
-    }
-
-    /// <summary>添加logging</summary>
-    /// <param name="builder"></param>
-    // private static void AddLogging(WebApplicationBuilder builder)
-    // {
-    //     builder.Logging.AddOpenTelemetry(lo =>
-    //     {
-    //         // 包含attributes字段，里面有RequestUrl，ConnectionId等信息
-    //         lo.IncludeScopes = true;
-    //         // var resourceBuilder = ResourceBuilder.CreateDefault().AddService(AppName, serviceVersion: AppVersion);
-    //         // lo.SetResourceBuilder(resourceBuilder);
-    //         lo.AddOtlpExporter(options =>
-    //         {
-    //             options.Endpoint = new Uri(_ocEndpoint);
-    //             options.Protocol = OtlpExportProtocol.Grpc;
-    //         });
-    //     });
-    // }
-
-    private static void AddLogging(OpenTelemetryBuilder openTelemetryBuilder)
-    {
-        openTelemetryBuilder.WithLogging(builder => builder
-            // .AddConsoleExporter()
-            .AddOtlpExporter((options,logRecordExportProcessorOptions) =>
-            {
-                options.Endpoint = new Uri(_ocEndpoint);
-                options.Protocol = OtlpExportProtocol.Grpc;
-            })
-        );
-    }
-
-    /// <summary>添加 metrics 指标数据</summary>
-    /// <param name="openTelemetryBuilder"></param>
-    private static void AddMetrics(OpenTelemetryBuilder openTelemetryBuilder)
-    {
+        // 指标
         openTelemetryBuilder
-            .WithMetrics(builder => builder
+            .WithMetrics(meterProviderBuilder => meterProviderBuilder
                 // .AddConsoleExporter((options, readerOptions) =>
                 // {
                 //     readerOptions.PeriodicExportingMetricReaderOptions.ExportIntervalMilliseconds = 1000;
                 // })
                 // debug用
                 .AddPrometheusExporter()
-                .AddOtlpExporter((options, readerOptions) =>
-                {
-                    options.Endpoint = new Uri(_ocEndpoint);
-                    options.Protocol = OtlpExportProtocol.Grpc;
-
-                    readerOptions.PeriodicExportingMetricReaderOptions.ExportIntervalMilliseconds = 1000;
-                })
+                // 默认60秒导出一次,但无法和UseOtlpExporter兼容 https://github.com/open-telemetry/opentelemetry-dotnet/blob/main/src/OpenTelemetry.Exporter.OpenTelemetryProtocol/README.md#enable-metric-exporter
+                // .AddOtlpExporter((options, readerOptions) =>
+                // {
+                //     readerOptions.PeriodicExportingMetricReaderOptions.ExportIntervalMilliseconds = 1000;
+                // })
                 .AddMeter(AppName)
                 .AddProcessInstrumentation()
                 .AddHttpClientInstrumentation()
@@ -106,14 +67,10 @@ public static class MyOpenTelemetryExtension
                     o.AddEventSources("System.Net.Sockets");
                 })
             );
-    }
 
-    /// <summary>添加 trace 追踪数据</summary>
-    /// <param name="openTelemetryBuilder"></param>
-    private static void AddTrace(OpenTelemetryBuilder openTelemetryBuilder)
-    {
-        openTelemetryBuilder.WithTracing(builder =>
-            builder.AddAspNetCoreInstrumentation(o => { o.Filter = r => r.Request.Path != "/healthz"; })
+        // 链路追踪
+        openTelemetryBuilder.WithTracing(tracerProviderBuilder =>
+            tracerProviderBuilder.AddAspNetCoreInstrumentation(o => { o.Filter = r => r.Request.Path != "/healthz"; })
                 .AddGrpcClientInstrumentation(o =>
                 {
                     // grpc基于http,如果为true,那么http就可以检测到grpc的traceid,产生一些activity.所以默认false关掉它.
@@ -128,35 +85,23 @@ public static class MyOpenTelemetryExtension
                 // .SetSampler(new TraceIdRatioBasedSampler(0.1))  // 采集1/10的指标
                 // debug用
                 // .AddConsoleExporter()
-                .AddOtlpExporter(o =>
-                {
-                    o.Endpoint = new Uri(_ocEndpoint);
-                    o.Protocol = OtlpExportProtocol.Grpc;
-                }));
-
-        // docker run -d --name zipkin -p 9411:9411 openzipkin/zipkin
-        // .AddZipkinExporter(o =>
-        // {
-        //     o.Endpoint = new Uri("http://8.142.70.33:9411/api/v2/spans");
-        // });
-
-        // docker run -d --name jaeger \
-        // -e COLLECTOR_ZIPKIN_HOST_PORT=:9411 \
-        // -e COLLECTOR_OTLP_ENABLED=true \
-        // -e JAEGER_SAMPLER_TYPE=const \
-        // -e JAEGER_SAMPLER_PARAM=1 \
-        // -e JAEGER_REPORTER_LOG_SPANS=true \
-        // -p 6831:6831/udp \
-        // -p 6832:6832/udp \
-        // -p 5778:5778 \
-        // -p 16686:16686 \
-        // -p 4317:4317 \
-        // -p 4318:4318 \
-        // -p 14250:14250 \
-        // -p 14268:14268 \
-        // -p 14269:14269 \
-        // -p 9411:9411 \
-        // jaegertracing/all-in-one:1.39
-        // Jaeger支持了OTLP,不再需要引入exporter依赖 https://www.jaegertracing.io/docs/1.48/apis/#opentelemetry-protocol-stable
+            );
     }
+
+    // private static void AddLogging(WebApplicationBuilder builder)
+    // {
+    //     builder.Logging.AddOpenTelemetry(lo =>
+    //     {
+    //         // 包含attributes字段，里面有RequestUrl，ConnectionId等信息
+    //         lo.IncludeScopes = true;
+    //         // var resourceBuilder = ResourceBuilder.CreateDefault().AddService(AppName, serviceVersion: AppVersion);
+    //         // lo.SetResourceBuilder(resourceBuilder);
+    //         lo.AddOtlpExporter(options =>
+    //         {
+    //             options.Endpoint = new Uri(_ocEndpoint);
+    //             options.Protocol = OtlpExportProtocol.Grpc;
+    //         });
+    //     });
+    // }
+
 }
