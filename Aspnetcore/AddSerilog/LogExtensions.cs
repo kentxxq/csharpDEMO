23a3b641fbd241e4785cb0ea20771286eb609ca3
kentxxq <805429509@qq.com>
2024-06-24 16:16:24 +0800

    log添加机器名和ip

--- Aspnetcore/AddSerilog/LogExtensions.cs
+++ Aspnetcore/AddSerilog/LogExtensions.cs
@@ -1,4 +1,6 @@
-﻿using AddSerilog.Common;
+﻿using System.Net;
+using System.Net.Sockets;
+using AddSerilog.Common;
 using Serilog;
 using Serilog.Events;
 using Serilog.Sinks.SystemConsole.Themes;
@@ -7,13 +9,19 @@ namespace AddSerilog;
 
 public static class LogExtensions
 {
+    public static readonly string DefaultLogTemplate =
+        "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz}|{Level:u3}|{SourceContext}|{IP}|{MachineName}|{ThreadName}|{ThreadId}|{Message:lj}{Exception}{NewLine}";
+
     private static LoggerConfiguration AddCommonConfig(this LoggerConfiguration loggerConfiguration)
     {
+        var ip = GetLocalIP();
         return loggerConfiguration
             .MinimumLevel.Is(LogEventLevel.Information)
             .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
             .MinimumLevel.Override("System", LogEventLevel.Warning)
             .Enrich.WithProperty("AppName", ThisAssembly.Project.AssemblyName)
+            .Enrich.WithMachineName()
+            .Enrich.WithProperty("IP",ip.ToString())
             .Enrich.When(logEvent => !logEvent.Properties.ContainsKey("SourceContext"),
                 enrichmentConfig => enrichmentConfig.WithProperty("SourceContext", "SourceContext"))
             .Enrich.When(logEvent => !logEvent.Properties.ContainsKey("ThreadName"),
@@ -34,8 +42,7 @@ public static class LogExtensions
                 retainedFileCountLimit: 1))
             .WriteTo.Async(l =>
                 l.Console(
-                    outputTemplate:
-                    "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz}|{Level:u3}|{SourceContext}|{ThreadName}|{ThreadId}|{Message:lj}{Exception}{NewLine}",
+                    outputTemplate:DefaultLogTemplate,
                     theme: AnsiConsoleTheme.Code));
     }
 
@@ -52,13 +59,13 @@ public static class LogExtensions
             )
             .WriteTo.Async(l =>
                 l.Console(
-                    outputTemplate: configuration["KLog:Console:OutputTemplate"] ??
-                                    "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz}|{Level:u3}|{SourceContext}|{ThreadName}|{ThreadId}|{Message:lj}{Exception}{NewLine}",
+                    outputTemplate: configuration["KLog:Console:OutputTemplate"] ??DefaultLogTemplate,
                     theme: AnsiConsoleTheme.Code)
             );
     }
 
-    public static LoggerConfiguration AddMyOpenTelemetry(this LoggerConfiguration loggerConfiguration,IConfiguration configuration)
+    public static LoggerConfiguration AddMyOpenTelemetry(this LoggerConfiguration loggerConfiguration,
+        IConfiguration configuration)
     {
         return loggerConfiguration.WriteTo.OpenTelemetry(options =>
             {
@@ -68,5 +75,28 @@ public static class LogExtensions
                 options.ResourceAttributes["job"] = ThisAssembly.Project.AssemblyName;
             }
         );
+    }
+
+
+    /// <summary>
+    /// 获取本机ipv4内网ip<br />
+    /// 如果网络不可用返回127.0.0.1<br />
+    /// 如果之前网络可用，可能会返回之前保留下来的ip地址
+    /// </summary>
+    /// <returns></returns>
+    public static IPAddress GetLocalIP()
+    {
+        try
+        {
+            using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0);
+            socket.Connect("223.5.5.5", 53);
+            var endPoint = socket.LocalEndPoint as IPEndPoint;
+            endPoint ??= new IPEndPoint(IPAddress.Loopback, 0);
+            return endPoint.Address;
+        }
+        catch (Exception)
+        {
+            return IPAddress.Loopback;
+        }
     }
 }
