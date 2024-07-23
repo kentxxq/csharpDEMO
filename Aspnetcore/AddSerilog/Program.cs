9bb3b28d9a7cf3c0d73d6f8d1f11b846762d6ce6
kentxxq <805429509@qq.com>
2024-07-23 16:40:48 +0800

    sync

--- Aspnetcore/AddSerilog/Program.cs
+++ Aspnetcore/AddSerilog/Program.cs
@@ -1,12 +1,7 @@
 using AddSerilog;
 using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
 
-var instanceId = Guid.NewGuid().ToString();
-
-
-// 在var app = builder.Build(); 后使用Log.Information,避免依赖没有注入的情况
 // 不建议使用CreateBootstrapLogger. 有时候会导致没有log,也和senparc.weixin的冲突...
 // 问题: https://github.com/serilog/serilog-sinks-file/issues/220  https://github.com/JeffreySu/WeiXinMPSDK/issues/3047
 // Log.Logger = new LoggerConfiguration()
@@ -14,37 +9,36 @@ var instanceId = Guid.NewGuid().ToString();
 //     .CreateBootstrapLogger();
 // Log.Information("日志初始化完成,正在启动服务...");
 
-try
-{
-    var enableOpentelemetry = builder.Configuration.GetValue("EnableOpenTelemetry", false);
 
-    // AddMyOpenTelemetry必须一起使用
+var builder = WebApplication.CreateBuilder(args);
+var instanceId = Guid.NewGuid().ToString();
+var enableOpentelemetry = builder.Configuration.GetValue("EnableOpenTelemetry", false);
+// AddMyOpenTelemetry必须一起使用
+// if (enableOpentelemetry)
+// {
+//     // 必须在AddMyOpenTelemetry之前,不能和下面的放在一起
+//     builder.AddMyOpenTelemetry(instanceId);
+// }
+builder.Services.AddSerilog((services, lc) => {
+    lc.AddCustomLogConfig(builder.Configuration);
+    // 必须和AddMyOpenTelemetry配合使用
     // if (enableOpentelemetry)
     // {
-    //     // 必须在AddMyOpenTelemetry之前,不能和下面的放在一起
-    //     builder.AddMyOpenTelemetry(instanceId);
+    //     lc.AddMyOpenTelemetry(builder.Configuration,instanceId);
     // }
+});
 
-    builder.Host.UseSerilog((serviceProvider, loggerConfiguration) =>
-    {
-        loggerConfiguration
-            .AddCustomLogConfig(builder.Configuration);
-        // 必须和AddMyOpenTelemetry配合使用
-        // if (enableOpentelemetry)
-        // {
-        //     loggerConfiguration.AddMyOpenTelemetry(builder.Configuration,instanceId);
-        // }
-    });
-
+try
+{
     builder.Services.AddControllers();
 
+    // 这行代码后使用Log.Information,避免依赖没有注入的情况
     var app = builder.Build();
 
     // 简化http输出
     app.UseSerilogRequestLogging();
 
     app.MapControllers();
-
     app.Run();
 }
 
