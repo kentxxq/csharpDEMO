dd51afc9c18eb59bf2225d1acfb34b504142dfcd
kentxxq <805429509@qq.com>
2024-09-10 17:22:30 +0800

    加上注释

--- PackageUsage/Cli/ConsoleAppDemo/Program.cs
+++ PackageUsage/Cli/ConsoleAppDemo/Program.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.Extensions.Hosting;
 
+// Host.CreateApplicationBuilder() 替代 Host.CreateDefaultBuilder() 因为 dotnet 的代码基调是从 callback 转向线性代码
+// 相关讨论参考
+// https://stackoverflow.com/questions/75229793/host-createdefaultbuilder-vs-host-createapplicationbuilder-in-net-platform-exte
+// https://github.com/dotnet/runtime/issues/61634
 var builder = Host.CreateApplicationBuilder();
 
-// 不再使用 new ServiceCollection() , 使用 builder.Services 替代
+// builder.configureService 变成 builder.Services 就是 callback 转向线性代码
 Console.WriteLine($"services: {builder.Services.Count}");
 
 var host = builder.Build();
