398bc94e1d02562db08a372ef9c436ec165cec68
kentxxq <805429509@qq.com>
2024-09-10 17:02:57 +0800

    控制台demo

---
+++ PackageUsage/Cli/ConsoleAppDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder();
+
+// 不再使用 new ServiceCollection() , 使用 builder.Services 替代
+Console.WriteLine($"services: {builder.Services.Count}");
+
+var host = builder.Build();
+
+await host.RunAsync();
